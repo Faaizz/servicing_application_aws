9953f158ece9653e8080cb6218efc8dbf5bd786b
Faaizz <fr33ziey@gmail.com>
2023-05-11 19:51:56 +0200

    ops/remove OIDC

--- terraform/pre_deployment/main.tf
+++ terraform/pre_deployment/main.tf
@@ -3,26 +3,3 @@ resource "aws_s3_bucket" "this" {
   bucket        = var.s3_bucket_name
   force_destroy = true
 }
-
-module "iam_github_oidc_role" {
-  source      = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
-  version     = "5.10"
-  name_prefix = "GitHubOIDCRole-"
-
-  subjects = ["${var.github_user}/${var.github_repo}:*"]
-
-  policies = {
-    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
-  }
-}
-
-module "iam_github_oidc_provider" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
-  version = "5.10"
-
-  url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com",
-  ]
-}
--- terraform/pre_deployment/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "gh_actions_role_arn" {
-  value       = module.iam_github_oidc_role.arn
-  description = "GitHub actions role ARN"
-}
